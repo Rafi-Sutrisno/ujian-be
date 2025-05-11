08fd6c199c3905c917e6fb9c27ea65ff5d5cdf60
Rafi Sutrisno <mrafis2003@gmail.com>
2025-05-11 19:58:20 +0700

    allow header for SEB config

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,6 +10,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		origin := c.Request.Header.Get("Origin")
+		// fmt.Println("Origin:", origin)
 		allowedOrigins := map[string]bool{
 			"https://your-frontend.com": true, // Change to your actual frontend domain
 			"http://localhost:3000":     true, // Allow local dev
@@ -19,10 +20,12 @@ func CORSMiddleware() gin.HandlerFunc {
 			c.Header("Access-Control-Allow-Origin", origin) // Allow only specific origins
 		}
 		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Accept, Origin, Cache-Control, X-Requested-With, X-SafeExamBrowser-BrowserExamKeyHash, x-safeexambrowser-configkeyhash, x-safeexambrowser-requesthash")
+
 		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == http.MethodOptions {
+			// fmt.Println("masuk sini") //here
 			c.AbortWithStatus(204)
 			return
 		}
