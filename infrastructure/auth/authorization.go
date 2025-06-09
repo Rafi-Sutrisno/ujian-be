566c614e7505c3a45aa9ae3e251478ff2cea60f4
Rafi Sutrisno <mrafis2003@gmail.com>
2025-06-09 11:15:43 +0700

    remove gin from auth repo

--- infrastructure/auth/authorization.go
+++ infrastructure/auth/authorization.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
@@ -103,7 +102,7 @@ func (r *authRepository) HasExamSession(ctx context.Context, examId, sessionId s
 	return err == nil, err
 }
 
-func (r *authRepository) CanStartExam(ctx context.Context, userId, examId string) (int64, error) {
+func (r *authRepository) CanStartExam(ctx context.Context, userAgent,requestHash, configKeyHash, fullURL, userId, examId string) (int64, error) {
 	inClass, err := r.IsUserInExamClass(ctx, userId, examId)
 	if err != nil {
 		return 0, err
@@ -119,10 +118,15 @@ func (r *authRepository) CanStartExam(ctx context.Context, userId, examId string
 	if !active {
 		return 0, errors.New("exam is not active (not in time interval)")
 	}
+
+	if err := r.ValidateSEBRequest( ctx, userAgent,requestHash, configKeyHash, fullURL, examId); err != nil {
+		return 0, fmt.Errorf("SEB validation failed: %w", err)
+	}
+
 	return timeleft, nil
 }
 
-func (r *authRepository) CanAccessExam(ctx context.Context, ginCtx *gin.Context, userId, examId string) error {
+func (r *authRepository) CanAccessExam(ctx context.Context, userAgent,requestHash, configKeyHash, fullURL, sessionID, userId, examId string) error {
 	inClass, err := r.IsUserInExamClass(ctx, userId, examId)
 	if err != nil {
 		return err
@@ -148,21 +152,23 @@ func (r *authRepository) CanAccessExam(ctx context.Context, ginCtx *gin.Context,
 		return errors.New("exam is not active")
 	}
 
-	sessionID, err := ginCtx.Cookie("session_id")
-	if err != nil {
-		return errors.New("you don't have session id to access the exam")
-	}
-	fmt.Println("ini session id dari cookie:", sessionID)
+	// sessionID, err := ginCtx.Cookie("session_id")
+	// if err != nil {
+	// 	return errors.New("you don't have session id to access the exam")
+	// }
+	// fmt.Println("ini session id dari cookie:", sessionID)
 
 	hasSession, err := r.HasExamSession(ctx, examId, sessionID)
 	if err != nil {
+		fmt.Println("ini error:", err)
 		return err
 	}
 	if !hasSession {
-		return errors.New("you don't have access to exam")
+		fmt.Println("masuk sini:")
+		return errors.New("you don't have access to exam (Session Invalid)")
 	}
 
-	if err := r.ValidateSEBRequest(ginCtx, ctx, examId); err != nil {
+	if err := r.ValidateSEBRequest(ctx, userAgent,requestHash, configKeyHash, fullURL, examId); err != nil {
 		return fmt.Errorf("SEB validation failed: %w", err)
 	}
 
@@ -199,23 +205,21 @@ func (r *authRepository) CanSeeExamResult(ctx context.Context, userId, examId st
 	return nil
 }
 
-func (r *authRepository) CanAccessProblem(ctx context.Context, ginCtx *gin.Context, userId, problemId string) error {
+func (r *authRepository) CanAccessProblem(ctx context.Context,userAgent,requestHash, configKeyHash, fullURL, sessionID, userId, problemId string) error {
 	user, err := r.GetUserById(ctx, userId)
 	if err != nil {
 		return err
 	}
-	fmt.Println("ini user:", user.RoleID)
 
 	if user.RoleID == 1 {
-		fmt.Println("masuk sini")
 		return nil
 	}
 
-	sessionID, err := ginCtx.Cookie("session_id")
-	if err != nil {
-		return errors.New("you don't have session id to access the problem")
-	}
-	fmt.Println("ini session id dari cookie:", sessionID)
+	// sessionID, err := ginCtx.Cookie("session_id")
+	// if err != nil {
+	// 	return errors.New("you don't have session id to access the problem")
+	// }
+	// fmt.Println("ini session id dari cookie:", sessionID)
 
     examSession, err := r.GetExamSessionBySessionId(ctx, sessionID)
 	if err != nil {
@@ -250,14 +254,14 @@ func (r *authRepository) CanAccessProblem(ctx context.Context, ginCtx *gin.Conte
 		return errors.New("you don't have access to exam")
 	}
 
-	if err := r.ValidateSEBRequest(ginCtx, ctx, examSession.ExamID); err != nil {
+	if err := r.ValidateSEBRequest( ctx,userAgent,requestHash, configKeyHash, fullURL, examSession.ExamID); err != nil {
 		return fmt.Errorf("SEB validation failed: %w", err)
 	}
 
 	return nil
 }
 
-func (r *authRepository) ValidateSEBRequest(GinCtx *gin.Context, ctx context.Context, examId string) error {
+func (r *authRepository) ValidateSEBRequest(ctx context.Context, userAgent,requestHash, configKeyHash, fullURL, examId string) error {
 	var exam entity.Exam
 	if err := r.db.WithContext(ctx).Where("id = ?", examId).Take(&exam).Error; err != nil {
 		return err
@@ -267,29 +271,29 @@ func (r *authRepository) ValidateSEBRequest(GinCtx *gin.Context, ctx context.Con
 		return nil // No SEB restrictions
 	}
 
-	userAgent := GinCtx.Request.UserAgent()
-	requestHash := GinCtx.GetHeader("X-SafeExamBrowser-RequestHash")
-	configKeyHash := GinCtx.GetHeader("X-Safeexambrowser-Configkeyhash")
+	// userAgent := GinCtx.Request.UserAgent()
+	// requestHash := GinCtx.GetHeader("X-SafeExamBrowser-RequestHash")
+	// configKeyHash := GinCtx.GetHeader("X-Safeexambrowser-Configkeyhash")
 
-	scheme := "http"
-	if GinCtx.Request.TLS != nil {
-		scheme = "https"
-	}
-	fullURL := fmt.Sprintf("%s://%s%s", scheme, GinCtx.Request.Host, GinCtx.Request.RequestURI)
+	// scheme := "http"
+	// if GinCtx.Request.TLS != nil {
+	// 	scheme = "https"
+	// }
+	// fullURL := fmt.Sprintf("%s://%s%s", scheme, GinCtx.Request.Host, GinCtx.Request.RequestURI)
 
 	// If not in header, fall back to request body (already parsed in controller)
-	var body struct {
-		BrowserExamKey string `json:"browser_exam_key"`
-		ConfigKey      string `json:"config_key"`
-	}
-	if err := GinCtx.ShouldBind(&body); err == nil {
-		if requestHash == "" {
-			requestHash = body.BrowserExamKey
-		}
-		if configKeyHash == "" {
-			configKeyHash = body.ConfigKey
-		}
-	}
+	// var body struct {
+	// 	BrowserExamKey string `json:"browser_exam_key"`
+	// 	ConfigKey      string `json:"config_key"`
+	// }
+	// if err := GinCtx.ShouldBind(&body); err == nil {
+	// 	if requestHash == "" {
+	// 		requestHash = body.BrowserExamKey
+	// 	}
+	// 	if configKeyHash == "" {
+	// 		configKeyHash = body.ConfigKey
+	// 	}
+	// }
 
 	// Validate based on provided keys
 	if exam.SEBBrowserKey != "" {
